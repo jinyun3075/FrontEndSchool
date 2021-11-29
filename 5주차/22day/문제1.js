9de6d41dcffefc3746fa6bbfde03935ddb2223df
YunJae Jin <64072136+jinyun3075@users.noreply.github.com>
2021-11-29 18:12:04 +0900

    JS 삼항연산자, 반복문, if문, switch, 이중포문, 배열 ....

---
+++ 5주차/22day/문제1.js
@@ -0,0 +1,27 @@
+// let sample = [10, 20 ,30, 5, 6, 3, 100, 110];
+// let temp = sample[0]
+
+// for(let i = 1; i<sample.length; i++){
+//     if(temp< sample[i]){
+//         temp = sample[i];
+//     }
+// }
+
+// let text= 'javascript';
+
+// for(let i = 0; i < text.length ; i++) {
+//     console.log(text[i], text[i+1]);
+// }
+
+// let value = [1, 4, 6, 8, 13, 17, 20, 21, 28];
+// let temp = value[1] - value[0];
+// let result = 0
+
+// for (let i = 0; i < value.length - 1; i++) {
+//     if (temp > (value[i+1] - value[i])){
+//         result = value[i+1] - value[i];
+//         temp =value[i+1] - value[i];
+//     }
+// }
+
+// console.log(result);
