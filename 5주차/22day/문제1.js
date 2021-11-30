4fe7888b80d000fbe522a1474fd8b00f96ddbf10
jinyunjae <jinyun3075@naver.com>
2021-11-30 21:03:53 +0900

    merge

--- 5주차/22day/문제1.js
+++ 5주차/22day/문제1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // let sample = [10, 20 ,30, 5, 6, 3, 100, 110];
 // let temp = sample[0]
 
@@ -25,32 +24,3 @@
 //     }
 // }
 
-=======
-// let sample = [10, 20 ,30, 5, 6, 3, 100, 110];
-// let temp = sample[0]
-
-// for(let i = 1; i<sample.length; i++){
-//     if(temp< sample[i]){
-//         temp = sample[i];
-//     }
-// }
-
-// let text= 'javascript';
-
-// for(let i = 0; i < text.length ; i++) {
-//     console.log(text[i], text[i+1]);
-// }
-
-// let value = [1, 4, 6, 8, 13, 17, 20, 21, 28];
-// let temp = value[1] - value[0];
-// let result = 0
-
-// for (let i = 0; i < value.length - 1; i++) {
-//     if (temp > (value[i+1] - value[i])){
-//         result = value[i+1] - value[i];
-//         temp =value[i+1] - value[i];
-//     }
-// }
-
->>>>>>> 71ea898230905638c61a99ddbe935800e16456d9
-// console.log(result);
